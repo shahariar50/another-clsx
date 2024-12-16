56a42799144d527c1effbec696567acb31f87789
Abdullah Al Sahariar <shahariar.touhid@gmail.com>
2024-12-16 06:00:53 +0600

    Added clsx main function

---
+++ src/index.js
@@ -0,0 +1,25 @@
+export default clsx = (...args) => {
+  const cls = "";
+  args.map((item) => {
+    cls && (cls += " ");
+    cls += mapClass(item);
+  });
+  return cls;
+};
+
+const mapClass = (value) => {
+  let cls = "";
+  if (typeof value === "string") {
+    cls += value;
+  } else if (typeof value === "number") {
+    cls += value.toString();
+  } else {
+    Object.entries(value).forEach(([key, value]) => {
+      if (value) {
+        if (cls) cls && (cls += " ");
+        cls += key;
+      }
+    });
+  }
+  return cls;
+};
